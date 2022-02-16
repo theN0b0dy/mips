8409df8f05106c009f762bc51d26d58ab318b1ba
AMS <amirreza.m.saki@gmail.com>
2022-02-16 21:05:43 +0330

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -6,7 +6,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 -- Bottazzi, Cristian - 2017 (https://github.com/cristian1604/)
 
--- Uncomment the following library declaration if instantiating
+-- Uncomment the following library declaration if instantiating.
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
